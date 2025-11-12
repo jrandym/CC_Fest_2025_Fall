b89b02ab6025d91251deae58c65df60e0b9626bb
jrandym <jrandy.macdonald@gmail.com>
2025-11-12 10:41:24 -0800

    Comment the circle animation code and update background color

--- Assignment#2.2/sketch.js
+++ Assignment#2.2/sketch.js
@@ -1,24 +1,28 @@
+// Starting position of the circle
 let x = 200;
 let  y = 200;
+// Velocity of the circle
 let vx = 3;
 let  vy = 2;
+// Radius of the circle
 let r = 25;
-
+// Setup function to create the canvas
 function setup() {
   createCanvas(400, 400);
 }
-
+// Draw function to animate the circle
 function draw() {
-  background(240);
-
+  background(222);
+// Update position of the circle based on velocity
   x += vx;
   y += vy;
-
+// Check for collision with canvas edges and reverse velocity if needed
   if (x < r || x > width - r) {
     vx *= -1;
   }
   if (y < r || y > height - r) {
     vy *= -1;
   }
+// Draw the circle at the updated position
   circle(x, y, r * 2);
 }
