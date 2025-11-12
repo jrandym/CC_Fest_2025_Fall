bdbd33159188f7f2ca09b17228d47dd86ebe63d1
jrandym <jrandy.macdonald@gmail.com>
2025-11-12 11:05:24 -0800

    Added clicker functionality to change size of circle

--- Assignment#2.2/sketch.js
+++ Assignment#2.2/sketch.js
@@ -1,9 +1,9 @@
 // Starting position of the circle
 let x = 200;
-let  y = 200;
+let y = 200;
 // Velocity of the circle
-let vx = 3;
-let  vy = 2;
+let vx = 2;
+let vy = 2;
 // Radius of the circle
 let r = 25;
 // Setup function to create the canvas
@@ -13,16 +13,29 @@ function setup() {
 // Draw function to animate the circle
 function draw() {
   background(222);
-// Update position of the circle based on velocity
+  // Update position of the circle based on velocity
   x += vx;
   y += vy;
-// Check for collision with canvas edges and reverse velocity if needed
+  // Check for collision with canvas edges and reverse velocity if needed
   if (x < r || x > width - r) {
     vx *= -1;
   }
   if (y < r || y > height - r) {
     vy *= -1;
   }
-// Draw the circle at the updated position
+  // Draw the circle at the updated position
   circle(x, y, r * 2);
 }
+// Create a clicker function to change the size of the circle if the mouse is clicked inside the circle
+function mousePressed() {
+  let d = dist(mouseX, mouseY, x, y);
+  if (d < r) {
+    // r = random(10, 50);
+    r += 5;
+    // Reset radius if it exceeds 50
+    if (r > 50) {
+      r = 25;
+    }
+  }
+}
+
