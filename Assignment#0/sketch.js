094426b7b755cb189062d21f4558aa5bc1187f86
jrandym <jrandy.macdonald@gmail.com>
2025-11-16 11:10:15 -0800

    Added 1) Assignment#0 - p5.js template project 2) Assignment#4 - p5.vscode starter project

---
+++ Assignment#0/sketch.js
@@ -0,0 +1,23 @@
+/*
+ * Global variables
+ */
+
+/*
+ * User-defined functions
+ */
+
+function preload() {
+
+}
+
+/*
+ * Built-in functions
+ */
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
